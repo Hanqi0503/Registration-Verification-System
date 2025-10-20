bff8c9bb6c3f846aefca4b6544e904b3f95b92ef
Yvette Wu <wu.yuyi@northeastern.edu>
2025-10-20 16:56:23 -0400

    fix: clean document service code

--- src/app/services/__init__.py
+++ src/app/services/__init__.py
@@ -1,7 +1,8 @@
 from .payment_service import payment_service
 from .registration_service import registration_service 
-
+from .document_service import identification_service
 __all__ = [
     "payment_service",
     "registration_service",
+    "identification_service",
 ]
