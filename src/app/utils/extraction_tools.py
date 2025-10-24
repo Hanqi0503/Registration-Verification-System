d34ca269632f892dd6df5f3920ee0b28f78778f6
Yvette Wu <wu.yuyi@northeastern.edu>
2025-10-24 15:24:51 -0400

    fix: keep registration service both available for legacy with ETransfer and new system with Zeffy

--- src/app/utils/extraction_tools.py
+++ src/app/utils/extraction_tools.py
@@ -12,3 +12,17 @@ def extract_form_id(slug):
     """
     match = re.search(r'/(\d+)', slug)
     return match.group(1) if match else None
+
+def extract_submission_id(file_upload_urls):
+    """
+    Extracts submission ID from file upload URLs.
+    
+    :param file_upload_urls: List of file upload URLs.
+    :return: Submission ID or None.
+    """
+    if file_upload_urls:
+        file_upload_url = file_upload_urls[0]
+        matches = re.findall(r'\d+', file_upload_url)
+        if len(matches) > 1:
+            return matches[1]
+    return None
