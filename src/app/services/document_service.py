60acfe759275fc402888bd8302408101f15f6190
Yvette Wu <wu.yuyi@northeastern.edu>
2025-11-05 11:16:42 -0500

    fix: fix the problem when wrong id but still pass the ocr verificaition

--- src/app/services/document_service.py
+++ src/app/services/document_service.py
@@ -215,7 +215,7 @@ def identification_service(image_url: str, register_info: dict) -> Identificatio
         id_info = {}
         if full_name and card_number:
             id_info = _get_id_info(texts, last_name,first_name, card_number)
-            if "full_name" not in id_info or "id_number" not in id_info:
+            if not info['full_name'] or not info['id_number']:
                 notify_manually_check = True
                 reasons.append(f"Full name or ID number does not match the input.")
                 valid = False
